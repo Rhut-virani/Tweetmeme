b11471111ce8637ef0cca63bbe304a2c340b71b2
Rhut Virani <rhut.virani@gmail.com>
2019-05-29 17:24:09 -0400

    before deploying to heroku

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,20 @@
       <Photogrid
         v-if="isShowing"
         :photos="imgUrl"
+        :next="next"
         @set-currentItem="setCurrentItem"
       />
+    </transition>
+
+    <!-- Next -->
+    <transition name="fade">
+    <Footer 
+        v-if="isShowing"
+        :next="next"
+        :dataurl="dataurl"
+        @get-next="getnext"
+        @get-prev="getprev"
+    />
     </transition>
   </div>
 </template>
@@ -20,12 +32,14 @@
 <script>
 import Nav from './components/Nav.vue';
 import Modal from './components/Modal.vue'
+import Footer from './components/Footer.vue'
 import Photogrid from './components/Photogrid.vue';
 import Twit from 'twit';
 import { isArray } from 'util';
+import { constants } from 'crypto';
 
 var   max_id = 0,
-      temp = "News",
+      temp = "news",
       query = "",
       tobject = { 
           consumer_key:         'mRS2Oj1xwemPhQyLPGK1xbkGN',
@@ -33,10 +47,9 @@ var   max_id = 0,
           access_token:         '519379540-kMBYnojtUAmOaGUrLiucu8YA5vElBy0kUMfv9TeQ',
           access_token_secret:  'rspNbbMIfvyg7ja1IL9HD0vKOJbz91tw9Pfpddq3rAhAG',
           timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-        },
+      },
       date = new Date(),
       hours = date.getHours(),
-      dataurl = [],
       T = new Twit(tobject);
     
 export default {
@@ -45,51 +58,79 @@ export default {
     Nav,
     Modal,
     Photogrid,
+    Footer,
   },
   data(){
     return{
       currentItem: {},
       imgUrl : [],
-      // dataurl: [],
+      dataurl: [],
       isShowing: true,
+      next: 0,
   }
   },
-  mounted: function (){
-    if(!!localStorage.data && (parseInt(localStorage.time) === hours)){
-      this.imgUrl = localStorage.data;
+  created: function () {
+    if(!!localStorage.data && (parseInt(JSON.parse(localStorage.getItem('time'))) === hours)){
+      this.dataurl = JSON.parse(localStorage.getItem('data'));
+      this.flatenarr();
     }
     else{
       this.getMemes(temp);
     }
   },
   methods:{
-    flatenarr: function (){
-      const newarray = dataurl.flat();
-      let ans = true;
-      let copy = [...newarray].map((e,index)=>{
-          ans = (!!e.entities.media.length && !e.possibly_sensitive) ? true : false;
-          let x = e.extended_entities.media[0].media_url
-          if(true){
-            return{
-              id: e.id + Math.floor(Math.random() * (index) + (index + 10)),
-              text: e.full_text,
-              url: x,
+    flatenarr: function (error) {
+      if(!error){
+        const newarray = this.dataurl;
+        let ans;
+        let x ;
+        const copy = newarray.map( (e,index) => {
+            ans = (!!e.entities.media.length && !e.possibly_sensitive) ? true : false;
+            x = e.extended_entities.media[0].media_url
+            if(ans){
+              return{
+                id: e.id + Math.floor(Math.random() * (index) + (index + 10)),
+                text: e.full_text,
+                url: x,
+              }
             }
-          }
-      });
-      this.imgUrl = copy;
-      this.isShowing = true;
-      if(localStorage.data){
-        localStorage.time = hours;
-        localStorage.data = this.imgUrl;
+        });
+
+        this.imgUrl = copy.filter((ele, i)=>{
+           if(i>this.next && i<(this.next+25)){
+             return ele;
+           }
+        });
+        this.isShowing = true;
+        if(!(localStorage.getItem('data'))){
+          localStorage.setItem('time', JSON.stringify(hours));
+          localStorage.setItem('data', JSON.stringify(this.dataurl));
+        }
       }
+      else if(error){
+        alert("No Results Found");
+      }
+
     },
+
+    getnext: function(){
+      this.next = this.next + 25;
+      window.scrollTo({top:0, left:0, behavior: 'smooth'});
+      this.flatenarr();
+    },
+
+    getprev: function(){
+      this.next = this.next - 25;
+      window.scrollTo({top:0, left:0, behavior: 'smooth'});
+      this.flatenarr();
+    },
+    
     resultpromise: function(){
         return (data) => {
           if(data.data.statuses.length === 0){
             throw "error";
           }
-          dataurl.push(...data.data.statuses);
+          this.dataurl.push(...data.data.statuses);
           max_id = data.data.statuses[data.data.statuses.length - 1].id;
         }
     },
@@ -103,35 +144,26 @@ export default {
           }
     },
     getMemes: function (text){
+      this.next = 0;
       this.isShowing = false;
-      dataurl = [];
+      this.dataurl = [];
       query = `${text} filter:images -filter:retweets`;
       T
       .get(
           'search/tweets', 
-          {q: query, count: 100 , tweet_mode:"extended", result_type:"mixed", lang:"en" }
+          {q: query, count: 100 , tweet_mode:"extended", result_type:"recent", lang:"en" }
       )
-      .catch((error)=>{
-        console.log(error);})
-      .then(this.resultpromise())
-      .catch((error)=>{
-        console.log(error);})
-      .then(this.promise())
-      .catch((error)=>{
-        console.log(error);})
       .then(this.resultpromise())
       .then(this.promise())
-      .catch((error)=>{
-        console.log(error);})
       .then(this.resultpromise())
       .then(this.promise())
-      .catch((error)=>{
-        console.log(error);})
       .then(this.resultpromise())
+      .then(this.promise())
+      .then(this.resultpromise())
+      .then((error) => {
+        this.flatenarr(error)})      
       .catch((error)=>{
-        console.log(error);})
-      .then(this.flatenarr);
-
+        this.flatenarr(error);});
     },
     setCurrentItem: function (photo){
       this.currentItem = photo;
@@ -142,7 +174,7 @@ export default {
 
 <style>
 .fade-enter-active, .fade-leave-active {
-  transition: opacity 0.25s ease-out;
+  transition: opacity 0.1s ease-out;
 }
 
 .fade-enter, .fade-leave-to {
---
+++ src/components/Footer.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="footer">
+    <div class="prev"
+        @click="$emit('get-prev')"
+        :class="{'disabled':(next <= 0)}"
+        :disabled="true"
+    > Prev</div>
+    <div class="next" 
+        v-on:click="$emit('get-next')"
+        :class="{'disabled':(next >= (dataurl.length-25))}"
+    >Next</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Footer",
+  props:{
+    next: {
+      type: Number,
+      default: 0,
+    },
+    dataurl:{
+      type: Array,
+      defualt: [],
+    }
+  }
+}
+</script>
+
+<style scoped>
+.footer{
+  width: 50vw;
+  height: 10vh;
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
+  margin-top: 2%;
+  text-align: center;
+}
+.prev, .next{
+  display: inline-block;
+  width: 10vw;
+  color: #e24040;
+  cursor: pointer;
+  outline: none;
+  -webkit-user-select: none;  /* Chrome all / Safari all */
+  -moz-user-select: none;     /* Firefox all */
+  -ms-user-select: none;      /* IE 10+ */
+  user-select: none; 
+  font-size: 1.2rem;
+  margin-right: 5%;
+}
+
+@media (min-width: 1199) {
+  .prev, .next {
+    position: fixed; 
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  .prev{
+    left: 0;
+  }
+  .next{
+    right: 0;
+  }
+}
+
+.disabled{
+  opacity: 0.5;
+  pointer-events: none;
+}
+
+</style>
+
