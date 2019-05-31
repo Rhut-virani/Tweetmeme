9bbe88338f7d19d43640de38aa808bf3975546a2
Rhut Virani <rhut.virani@gmail.com>
2019-05-31 15:10:41 -0400

    backend api

--- server.js
+++ server.js
@@ -1,12 +1,142 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
 
-app = express();
+const express = require('express'),
+      app = express(),
+      bodyParser = require('body-parser'),
+      Twit = require('twit');
+var   serveStatic = require('serve-static');
+
+const PORT = process.env.PORT || 5000;
+
+// parse application/x-www-form-urlencoded
+    app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+    app.use(bodyParser.json())
+
+// adding the lines for cors so that incoming connection are handled properly 
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
+
+
 app.use(serveStatic(__dirname + "/dist"));
 
-var port = process.env.PORT || 8080;
 
-app.listen(port);
+let dataurl = [],
+    imgUrl= [],
+    temp = "dhoni",
+    query = "",
+    tobject = { 
+        consumer_key:         'JVXJ5L7963GEa5EM9IKNPd3iQ',
+        consumer_secret:      'vSd4uL6qrGFhTnw2v0VjFhIPatHfoy2gti5W28jGcuKoEXM2Ei',
+        access_token:         '519379540-MgU49G2LXZ0xp4nJMpdXyQMyhNycqt9mKJn3aKB4',
+        access_token_secret:  'b6Q4dTKSkcMXLsNd0XE2PxodyXifGS3sGxs7NrbsdoE0V',
+        timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    },
+    T = new Twit(tobject);
 
-console.log('server started '+ port);
+    
+
+app.get('/data', (req, res)=>{
+    let search = req.query.search || false;
+    async function sendata(){
+        if(!!search){
+            let result = await getMemes(search);
+            await res.send(result);
+        }
+        else{
+            res.send(imgUrl);
+        }
+    }
+    sendata();
+})
+
+const flatenarr = (error) =>{
+        if(!error){
+            const newarray = Array.from(dataurl);
+            console.log(newarray[0].entities.media[0].media_url);
+            let ans;
+            let x ;
+            // imgUrl = newarray.filter( (tweet,index) => {
+            //     // ans = (!!e.entities.media.length && !e.possibly_sensitive) ? true : false;
+            //     // x = e.extended_entities.media[0].media_url;
+            //     if(true){
+            //         return{
+            //         id: tweet.id + Math.floor(Math.random() * (index) + (index + 10)),
+            //         text: tweet.full_text,
+            //         url: tweet.entities.media.media_url,
+            //         }
+            //     }
+            // });
+            let copy  = newarray.map((tweet,index) => {
+                // ans = (!!e.entities.media.length && !e.possibly_sensitive) ? true : false;
+                // x = e.extended_entities.media[0].media_url;        
+                if(index === 1){
+                    console.log(tweet);
+                }            
+                    imgUrl.push(
+                        [id= tweet.id + Math.floor(Math.random() * (index) + (index + 10)),
+                        text= tweet.full_text ,
+                        url = tweet.entities.media.media_url]
+                        );
+            })
+        }
+        else if(error){
+            console.log(error);
+        }
+  };
+
+
+const resultpromise = ()=>{
+    return (data) => {
+    if(data.data.statuses.length === 0){
+        throw `error${max_id}`;
+    }
+    let result = JSON.stringify(data.data.statuses)
+    dataurl.push(...result);
+    max_id = result[data.data.statuses.length - 1].id;
+    }
+};
+const promise = () => {
+    return ()=>{
+    if(max_id){
+        return T.get(
+        'search/tweets', 
+            {q: query, count: 100, max_id: max_id, tweet_mode:"extended", result_type:"mixed", lang:"en" }
+        )}
+    }
+}
+
+async function getMemes(text){
+        dataurl = [];
+        query = `${text} filter:media -filter:retweets`;
+        await T
+        .get(
+            'search/tweets', 
+            {q: query, count: 100 , tweet_mode:"extended", result_type:"mixed", lang:"en" }
+        )
+        .then(resultpromise())
+        .then(promise())
+        .then(resultpromise())
+        .then(promise())
+        .then(resultpromise())
+        .then(promise())
+        .then(resultpromise())
+        .then(()=>{
+            flatenarr()
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+        return await imgUrl;
+};
+
+getMemes(temp);
+
+
+
+
+app.listen(PORT);
+console.log('server started '+ PORT);
--- src/App.vue
+++ src/App.vue
@@ -37,20 +37,12 @@ import Photogrid from './components/Photogrid.vue';
 import Twit from 'twit';
 import { isArray } from 'util';
 import { constants } from 'crypto';
+import axios from 'axios';
 
-var   max_id = 0,
-      temp = "news",
-      query = "",
-      tobject = { 
-          consumer_key:         'mRS2Oj1xwemPhQyLPGK1xbkGN',
-          consumer_secret:      'bXrlMg1cEKxgG7CQ17wtf4hZtygzKyOpETjevsU7BNmXSCopIX',
-          access_token:         '519379540-kMBYnojtUAmOaGUrLiucu8YA5vElBy0kUMfv9TeQ',
-          access_token_secret:  'rspNbbMIfvyg7ja1IL9HD0vKOJbz91tw9Pfpddq3rAhAG',
-          timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-      },
-      date = new Date(),
+
+let   date = new Date(),
       hours = date.getHours(),
-      T = new Twit(tobject);
+      $axios = axios;
     
 export default {
   name: 'App',
@@ -69,105 +61,48 @@ export default {
       next: 0,
   }
   },
-  created: function () {
-    if(!!localStorage.data && (parseInt(JSON.parse(localStorage.getItem('time'))) === hours)){
-      this.dataurl = JSON.parse(localStorage.getItem('data'));
-      this.flatenarr();
-    }
-    else{
-      this.getMemes(temp);
-    }
+  mounted: function () {
+    setTimeout(() => {
+      this.getMemes();      
+    }, 2000);
   },
   methods:{
-    flatenarr: function (error) {
-      if(!error){
-        const newarray = this.dataurl;
-        let ans;
-        let x ;
-        const copy = newarray.map( (e,index) => {
-            ans = (!!e.entities.media.length && !e.possibly_sensitive) ? true : false;
-            x = e.extended_entities.media[0].media_url
-            if(ans){
-              return{
-                id: e.id + Math.floor(Math.random() * (index) + (index + 10)),
-                text: e.full_text,
-                url: x,
-              }
-            }
-        });
-
+    sortresults: function(){
+        const copy = Array.from(this.dataurl);
         this.imgUrl = copy.filter((ele, i)=>{
            if(i>this.next && i<(this.next+25)){
              return ele;
            }
         });
         this.isShowing = true;
-        if(!(localStorage.getItem('data'))){
-          localStorage.setItem('time', JSON.stringify(hours));
-          localStorage.setItem('data', JSON.stringify(this.dataurl));
-        }
-      }
-      else if(error){
-        alert("No Results Found");
-      }
-
     },
-
     getnext: function(){
       this.next = this.next + 25;
       window.scrollTo({top:0, left:0, behavior: 'smooth'});
-      this.flatenarr();
+      this.sortresults();
     },
-
     getprev: function(){
       this.next = this.next - 25;
       window.scrollTo({top:0, left:0, behavior: 'smooth'});
-      this.flatenarr();
-    },
-    
-    resultpromise: function(){
-        return (data) => {
-          if(data.data.statuses.length === 0){
-            throw "error";
-          }
-          this.dataurl.push(...data.data.statuses);
-          max_id = data.data.statuses[data.data.statuses.length - 1].id;
-        }
-    },
-    promise: function(){
-        return ()=>{
-          if(max_id){
-              return T.get(
-            'search/tweets', 
-                {q: query, count: 100, max_id: max_id, tweet_mode:"extended", result_type:"mixed", lang:"en" }
-            )}
-          }
+      this.sortresults();
     },
     getMemes: function (text){
       this.next = 0;
       this.isShowing = false;
-      this.dataurl = [];
-      query = `${text} filter:images -filter:retweets`;
-      T
-      .get(
-          'search/tweets', 
-          {q: query, count: 100 , tweet_mode:"extended", result_type:"recent", lang:"en" }
-      )
-      .then(this.resultpromise())
-      .then(this.promise())
-      .then(this.resultpromise())
-      .then(this.promise())
-      .then(this.resultpromise())
-      .then(this.promise())
-      .then(this.resultpromise())
-      .then((error) => {
-        this.flatenarr(error)})      
+      $axios.get('http://localhost:5000/data', { params:{search:text}})
+      .then((result) => {
+        console.log(result.data);
+        this.dataurl = result.data;
+      }) 
+      .then(() => {
+        this.sortresults();
+      })      
       .catch((error)=>{
-        this.flatenarr(error);});
+        console.log(error)});
     },
     setCurrentItem: function (photo){
       this.currentItem = photo;
-    }
+    },
   }
 }
 </script>
